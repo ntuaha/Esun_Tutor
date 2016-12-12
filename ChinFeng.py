2ca31ea67e0aa60d3f65d642c5dd902f41ad341a
Cheng-Yu Lin <ntuaha@gmail.com>
2016-12-12 17:25:31 +0800

    第一版

---
+++ ChinFeng.py
@@ -0,0 +1,21 @@
+class Corporation:
+  def name(self,name):
+    self.name=name
+    print self.name
+
+  def establish_year(self,year):
+    self.establish_year=year
+    print self.establish_year
+
+  def Industry(self,industry):
+    self.industry=industry
+    print self.industry
+
+  def management(self,management):
+    self.management=manegement
+    print self.name + 'establish in' + self.establish_year 
+
+
+Esun=Corporation()
+Esun.name('Esun')
+Esun.establish_year('1982')
